b339a1545e7b7c5a4e0f30059c0ad725bcefc8f8
Jyoshna_2021 <jyoshna.s@tringapps.com>
2022-06-15 11:04:55 +0530

    MVVM

--- app/src/main/java/com/example/sorted_list_view/MainViewModelFactory.kt
+++ app/src/main/java/com/example/sorted_list_view/MainViewModelFactory.kt
@@ -3,9 +3,9 @@ package com.example.sorted_list_view
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class MainViewModelFactory(private val notesRepository: NotesRepository) : ViewModelProvider.Factory {
+class MainViewModelFactory(private val notesRepository: NotesRepository) :
+    ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return MainViewModel(notesRepository ) as T
-
+        return MainViewModel(notesRepository) as T
     }
 }
